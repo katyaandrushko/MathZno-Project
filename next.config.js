1c4c6cea22b205d0e6f23e56728c30cf1e96a28f
katyaandrushko <katyaandrushko14@gmail.com>
2024-01-25 16:32:37 +0200

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {}
 const nextConfig = {
    output: 'export',
 }
-// module.exports = nextConfig
 
-module.exports = {
-   webpack5: true,
-   webpack: (config) => {
-      config.resolve.fallback = {
-         fs: false,
-      }
+module.exports = nextConfig
 
-      return config
-   },
-}
+// module.exports = {
+//    webpack5: true,
+//    webpack: (config) => {
+//       config.resolve.fallback = {
+//          fs: false,
+//       }
+
+//       return config
+//    },
+// }
